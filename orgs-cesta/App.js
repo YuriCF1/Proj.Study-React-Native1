ddd170003d7236b94bc34d94077fb666e666f2fc
Yuri <yurifdev@gmail.com>
2023-04-05 17:52:30 -0300

    Implementing the new component

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import Cesta from "./src/screens/Cesta";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>O primeiro app!</Text>
+      <Cesta />
       <StatusBar style="auto" />
     </View>
   );
