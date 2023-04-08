4cb03252362424ca60eaf49ede5bb24069b4d4e9
Yuri <yurifdev@gmail.com>
2023-04-08 18:37:07 -0300

    Comenting to test

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,22 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
+// SafeAreaView = Nos iphones, faz com que a área seja evitada, criando uma margem
 
 import Cesta from "./src/screens/Cesta";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    // <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
