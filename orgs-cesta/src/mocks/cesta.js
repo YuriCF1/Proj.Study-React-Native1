a902f7ecaa20ce9ae1cd4c96ab69b1bb345aa4b1
Yuri <yurifdev@gmail.com>
2023-05-06 16:54:23 -0300

    Creating mocks

---
+++ orgs-cesta/src/mocks/cesta.js
@@ -0,0 +1,17 @@
+import logo from "../../assets/logo.png";
+
+const cesta = {
+  topo: {
+    titulo: "Detalhe da cesta",
+  },
+  detalhes: {
+    nome: "Cesta de verduras",
+    logoFazenda: logo,
+    nomeFazenda: "Jenny Jack Farm",
+    descricao:
+      "Uma cesta com ótimos produtos selecionados da fazenda, direto para sua cozinha",
+    preco: "R$ 40,00",
+  },
+};
+
+export default cesta;
