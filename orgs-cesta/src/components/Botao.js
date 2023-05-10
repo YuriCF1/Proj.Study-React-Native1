5b6fb36de5bbe6ff798259d942cb81566ed3045e
Yuri <yurifdev@gmail.com>
2023-05-10 20:52:10 -0300

    Creating a component of button

---
+++ orgs-cesta/src/components/Botao.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import {TouchableOpacity} from "react-native"
+import Texto from './Texto'
+
+const Botao = ({children}) => {
+  return (
+    <TouchableOpacity style={estilos.botao}>
+        <Texto style={estilos.textoBotao}>{children}</Texto>
+    </TouchableOpacity>
+  )
+}
+
+const estilos = StyleSheet.create({
+    botao: {
+        marginTop: 16,
+        backgroundColor: "#2A9F85",
+        paddingVertical: 16,
+        borderRadius: 12,
+      },
+      textoBotao: {
+        textAlign: "center",
+        color: "#ffffff",
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: "bold"
+      },
+})
+
+export default Botao
