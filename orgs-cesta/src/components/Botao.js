be5bdcac33d1395dd937ce66692144b0a4f9e4cf
Yuri <yurifdev@gmail.com>
2023-05-10 22:38:44 -0300

    Fixing bug in component

--- orgs-cesta/src/components/Botao.js
+++ orgs-cesta/src/components/Botao.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {TouchableOpacity} from "react-native"
+import {TouchableOpacity, StyleSheet} from "react-native"
 import Texto from './Texto'
 
 const Botao = ({children}) => {
