793138408a9e84886072dd0f6cb323214b37148a
Yuri <yurifdev@gmail.com>
2023-05-19 18:48:44 -0300

    Adding items mocking

--- orgs-cesta/src/mocks/cesta.js
+++ orgs-cesta/src/mocks/cesta.js
@@ -1,5 +1,12 @@
 import logo from "../../assets/logo.png";
 
+//Images itens
+import tomateImg from "../../assets/frutas/Tomate.png";
+import pepinoImg from "../../assets/frutas/Pepino.png";
+import brocolisImg from "../../assets/frutas/Brócolis.png";
+import batataImg from "../../assets/frutas/Batata.png";
+import aboboraImg from "../../assets/frutas/Abóbora.png";
+
 const cesta = {
   topo: {
     titulo: "Detalhe da cesta",
@@ -11,7 +18,32 @@ const cesta = {
     descricao:
       "Uma cesta com ótimos produtos selecionados da fazenda, direto para sua cozinha",
     preco: "R$ 40,00",
-    botao: "Comprar"
+    botao: "Comprar",
+  },
+  itens: {
+    titulo: "Itens da cesta",
+    lista: [
+      {
+        nome: "Tomate",
+        image: tomateImg,
+      },
+      {
+        nome: "Brócolos",
+        image: brocolisImg,
+      },
+      {
+        nome: "Batata",
+        image: batataImg,
+      },
+      {
+        nome: "Pepino",
+        image: pepinoImg,
+      },
+      {
+        nome: "Abóbora",
+        image: aboboraImg,
+      },
+    ],
   },
 };
 
